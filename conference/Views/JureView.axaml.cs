fa44e7e3312362ed147cee262b740fa412b7acd2
Bobarik31p <kbobarik@mail.ru>
2025-03-05 00:10:06 +0300

    add jure view

---
+++ conference/Views/JureView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace conference.Views;
+
+public partial class JureView : UserControl
+{
+    public JureView()
+    {
+        InitializeComponent();
+    }
+}
